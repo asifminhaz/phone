15f284cba343eabc4fd4d8e3b5b5abf7856dd333
asifminhaz <minhazurrahmanasif40@gmail.com>
2022-03-01 00:41:12 +0600

    search bar added

---
+++ js/app.js
@@ -0,0 +1,8 @@
+const searchPhone = () => {
+    const searchField = document.getElementById('search-field');
+    const searchText = searchField.value;
+    // console.log(searchText);
+
+    searchField.value = '';
+
+}
